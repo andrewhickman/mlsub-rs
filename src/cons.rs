8a207a04a09492fe4783e4fb012f315a545e7400
Andrew Hickman <andrew.hickman1@sky.com>
2019-01-20 14:58:36 +0000

    lazily initialize constructor hashmap

--- src/cons.rs
+++ src/cons.rs
@@ -1,8 +1,10 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
 use std::hash::{BuildHasherDefault, Hash};
+use std::iter::Flatten;
+use std::option;
 
-use im::hashmap::{self, Entry, HashMap};
+use im::hashmap::{Entry, HashMap, Values};
 use lazy_static::lazy_static;
 use seahash::SeaHasher;
 
@@ -20,12 +22,12 @@ pub trait Constructor: PartialOrd + Clone {
 
 #[derive(Clone, Debug)]
 pub(crate) struct ConstructorSet<C: Constructor> {
-    set: HashMap<C::Component, C, BuildHasherDefault<SeaHasher>>,
+    set: Option<HashMap<C::Component, C, BuildHasherDefault<SeaHasher>>>,
 }
 
 impl<C: Constructor> ConstructorSet<C> {
     pub(crate) fn add_pos(&mut self, con: Cow<C>) {
-        match self.set.entry(con.component()) {
+        match self.set().entry(con.component()) {
             Entry::Occupied(mut entry) => entry.get_mut().join(&con),
             Entry::Vacant(entry) => {
                 entry.insert(con.into_owned());
@@ -34,7 +36,7 @@ impl<C: Constructor> ConstructorSet<C> {
     }
 
     pub(crate) fn add_neg(&mut self, con: Cow<C>) {
-        match self.set.entry(con.component()) {
+        match self.set().entry(con.component()) {
             Entry::Occupied(mut entry) => entry.get_mut().meet(&con),
             Entry::Vacant(entry) => {
                 entry.insert(con.into_owned());
@@ -43,13 +45,13 @@ impl<C: Constructor> ConstructorSet<C> {
     }
 
     pub(crate) fn join(&mut self, other: &Self) {
-        for con in other.set.values() {
+        for con in other {
             self.add_pos(Cow::Borrowed(con));
         }
     }
 
     pub(crate) fn meet(&mut self, other: &Self) {
-        for con in other.set.values() {
+        for con in other {
             self.add_neg(Cow::Borrowed(con));
         }
     }
@@ -60,25 +62,27 @@ impl<C: Constructor> ConstructorSet<C> {
             Polarity::Neg => self.meet(other),
         }
     }
-}
 
-impl<C: Constructor> Default for ConstructorSet<C> {
-    fn default() -> Self {
+    fn set(&mut self) -> &mut HashMap<C::Component, C, BuildHasherDefault<SeaHasher>> {
         lazy_static! {
             static ref HASHER: HashMap<(), (), BuildHasherDefault<SeaHasher>> = HashMap::default();
         }
 
-        ConstructorSet {
-            set: HASHER.new_from(),
-        }
+        self.set.get_or_insert_with(|| HASHER.new_from())
+    }
+}
+
+impl<C: Constructor> Default for ConstructorSet<C> {
+    fn default() -> Self {
+        ConstructorSet { set: None }
     }
 }
 
 impl<'a, C: Constructor> IntoIterator for &'a ConstructorSet<C> {
     type Item = &'a C;
-    type IntoIter = hashmap::Values<'a, C::Component, C>;
+    type IntoIter = Flatten<option::IntoIter<Values<'a, C::Component, C>>>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.set.values()
+        self.set.as_ref().map(HashMap::values).into_iter().flatten()
     }
 }
