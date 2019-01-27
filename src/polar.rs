d5ca6d1d2379020ebabc1987d9abffe2a6a0e0d9
Andrew Hickman <andrew.hickman1@sky.com>
2019-01-27 14:25:21 +0000

    reorder

--- src/polar.rs
+++ src/polar.rs
@@ -1,9 +1,9 @@
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Ty<C, V> {
+    Zero,
+    Add(Box<Ty<C, V>>, Box<Ty<C, V>>),
     UnboundVar(V),
     BoundVar(usize),
     Constructed(C),
-    Add(Box<Ty<C, V>>, Box<Ty<C, V>>),
-    Zero,
     Recursive(Box<Ty<C, V>>),
 }
